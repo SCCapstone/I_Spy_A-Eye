2972e4030e0028a0c1502d9921a7092dbca02df4
Danielle Higgins <higdan4024@gmail.com>
2022-11-01 21:01:38 -0400

    added cart and order screens to screens folder

---
+++ screens/CartScreen.js
@@ -0,0 +1,12 @@
+import { Text, StyleSheet } from 'react-native';
+
+export default function CartScreen() {
+
+    return(
+        <Text>Cart</Text>
+    )
+}
+
+const style = StyleSheet.create({
+
+})
