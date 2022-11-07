87833aef5ac075a31e59877f5d02d62f6bb00ecf
Danielle Higgins <higdan4024@gmail.com>
2022-11-07 00:38:41 -0500

    Added a small outline and design for orders screen

--- screens/OrdersScreen.js
+++ screens/OrdersScreen.js
@@ -1,12 +1,37 @@
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, ScrollView, View } from 'react-native';
 
 export default function OrdersScreen() {
 
     return(
-        <Text>Orders</Text>
+        <ScrollView>
+          {/*Header*/}
+          <Text style={style.header}>Orders</Text>
+
+          {/*Horizontal line*/}
+          <View
+              style={{
+                borderBottomColor: 'black',
+                borderBottomWidth: 10,
+                marginTop: 5
+              }}
+            />
+            <View
+              style={{
+                borderBottomColor: 'lightgrey',
+                borderBottomWidth: 5
+              }}
+            />
+
+            {/*List of orders*/}
+        </ScrollView>
     )
 }
 
 const style = StyleSheet.create({
-
+  header: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: '45',
+    marginTop: 25,
+  },
 })
