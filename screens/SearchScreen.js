16ba5fdcb1202ae925732f2402f9a1191073bc3c
Zt572 <ztyoung@email.sc.edu>
2022-11-11 20:38:32 -0500

    First pass at search bar with name search #28

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,13 +1,77 @@
 import * as React from "react";
-import { Text, View, StyleSheet, TouchableOpacity, Image } from "react-native";
+import { Alert, Image, Pressable, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native";
 import globalStyle from "../globalStyle";
 
+
 export default class Page1 extends React.Component {
+  constructor(props) {
+    super(props);
+    // Set up default state for search bar input
+    this.state = {
+      input: "",
+      location: "LOCATION_ID_PLACEHOLDER",
+      filters: {country: null, inStock: true, onSale: false, favorited: false},
+      sort: null,
+      latestResults: null,
+    };
+  }
+
   render() {
     return (
       <SafeAreaView style={globalStyle.wholeScreen}>
-        <Text>Search</Text>
+        {/* Search Header */}
+        <View style={{marginHorizontal:15}}>
+          <Text style={globalStyle.headerText}>Search</Text>
+
+          {/* Row 1: Text Input and Search Button */}
+          <View style={globalStyle.headerButtonRow}>
+            {/* Search Bar Input */}
+            <TextInput 
+              style={[globalStyle.inputContainer, {flex: 3.2, marginRight: 10, fontWeight: 'bold', fontSize: 18}]}
+              placeholder="I'm looking for..."
+              placeholderTextColor={'#000000'}
+              onChangeText={newInput => this.setState({input: newInput.trim()})}
+              />
+            {/* Search Button */}
+            <Pressable style={styles.searchButtonStyle}
+              onPress={() => {
+                  let searchResults = searchProducts(this.state);
+                  if(searchResults != null) this.setState({latestResults: searchResults});
+                }
+                }
+              >
+              <Text style={styles.searchButtonText}>Search</Text>
+            </Pressable>
+          </View>
+
+          {/* Row 2: Filter, Sort, and Scan Buttons (TODO: Add functionality and relevant popup menus) */}
+          <View style={[globalStyle.headerButtonRow, {marginVertical: 10, marginHorizontal:-5}]}>
+            {/* Filter Button */}
+            <Pressable style={globalStyle.headerButtonStyle}>
+              <Text style={globalStyle.headerButtonText}>Filter</Text>
+            </Pressable>
+            {/* Sort Button */}
+            <Pressable style={globalStyle.headerButtonStyle}>
+              <Text style={globalStyle.headerButtonText}>Sort</Text>
+            </Pressable>
+            {/* Scan Button */}
+            <Pressable style={globalStyle.headerButtonStyle}>
+              <Text style={globalStyle.headerButtonText}>Scan</Text>
+            </Pressable>
+          </View>
+        </View>
+
+        {/* Horizontal line separator */}
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <View style={{flex: 1, height: 1, borderColor: 'black', borderBottomWidth: 10}} />
+        </View>
+
+        {/* Horizontal line separator's drop shadow */}
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <View style={{flex: 1, height: 1, borderColor: '#cccccc', borderBottomWidth: 3}} />
+        </View>
+
         <View style={globalStyle.container}>
           <View style={globalStyle.buttons}>
             <TouchableOpacity onPress={() => this.props.pageChange(1)}>
@@ -53,4 +117,71 @@ export default class Page1 extends React.Component {
   }
 }
 
-const styles = StyleSheet.create({});
+// Returns true if the input string meets the API's filter.term paramater requirements.
+function validInput(inputText) {
+  // Search terms limited to max of 8 words (separated by spaces). Must also be >= 3 characters
+  if(inputText.length<3) return false;
+
+  const terms = inputText.split(/\s+/); // Split inputText into separate terms using regex
+  if(terms.length>8) return false;
+
+  return true;
+}
+
+// Input: class state. Returns JSON response if the API call is successful, null otherwise. TO-DO: Filter/sort results after fetch
+async function searchProducts(state) {
+  // Confirm input is valid before querying
+  if(!validInput(state.input)) {
+    showAlert("Invalid Input", "Input must be 3 characters or more, and have at most 8 words.");
+    return null;
+  }
+
+  // Build query
+  let callURL = `https://api.kroger.com/v1/products?filter.term=${state.input}&filter.locationId=${state.location}$filter.fulfillment=dth`;
+
+  // Fetch results
+  let options = {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Authorization': `Bearer AUTH_TOKEN_PLACEHOLDER`
+    }
+  }
+
+  let response = await fetch(callURL, options);
+  let responseJSON = await response.json();
+
+  // If failed request, Alert the user and return null
+  if(!response.ok) {
+    showAlert('Error: ' + response.status.toString(), responseJSON.error_description);
+    return null;
+  }
+  return responseJSON;
+
+
+  // Input: Strings for the alert's title and alert's message. Displays a simple, cancellable alert with the given title and message
+  function showAlert(alertTitle, alertMsg) {
+    Alert.alert(
+      alertTitle,
+      alertMsg,
+      [{text: "OK"}],
+      {cancelable: true}
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+    searchButtonStyle: {
+      flex: 1,
+      backgroundColor: 'black',
+      borderRadius: 20,
+      paddingVertical: 17,
+      paddingHorizontal: 10,
+    },
+    searchButtonText:{
+      color: 'white',
+      fontWeight: 'bold',
+      fontSize: 20,
+      textAlign: 'center',
+    },
+})
