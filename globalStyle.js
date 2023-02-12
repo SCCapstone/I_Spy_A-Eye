d4585ff9e1a6992c18411e848d4a2431d06576cc
M-Valentino <mike9377mike9377@gmail.com>
2023-02-12 01:10:13 -0500

    Revert "#81 created login screen and began to set up firebase"
    This reverts commit 9c94aa9bc9a5bf6cfaea094ed156339f40a469f6.
    
    reverting accidental push to main

--- globalStyle.js
+++ globalStyle.js
@@ -1,75 +1,64 @@
-import { StyleSheet } from 'react-native'
+import {StyleSheet} from 'react-native'
 
 
-const globalStyle = StyleSheet.create({
-  wholeScreen: {
-    backgroundColor: '#fff',
-    height: '100%',
-  },
-  buttons: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    justifyContent: 'space-evenly',
-  },
-  container: {
-    position: 'absolute',
-    bottom: 0,
-    width: "100%",
-    borderWidth: 5,
-    borderRadius: 999,
-    backgroundColor: '#fff',
-    padding: 2,
-  },
-  icon: {
-    width: 36,
-    height: 36,
-    alignSelf: 'center'
-  },
-  headerButtonRow: {
-    alignContent: 'space-between',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  headerButtonStyle: {
-    flex: 1,
-    backgroundColor: 'black',
-    borderRadius: 20,
-    paddingVertical: 8,
-    paddingHorizontal: 20,
-    marginHorizontal: 4,
-  },
-  headerButtonText: {
-    color: 'white',
-    fontWeight: 'bold',
-    fontSize: 20,
-    textAlign: 'center',
-  },
-  headerDivider: {
-    borderBottomColor: 'black',
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
-  headerText: {
-    fontSize: 45,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  inputContainer: {
-    borderWidth: 7,
-    borderRadius: 20,
-    paddingVertical: 5,
-    paddingHorizontal: 25,
-  },
-  loginSignUpInputContainer: {
-    borderWidth: 7,
-    borderRadius: 20,
-    paddingVertical: 5,
-    paddingHorizontal: 25,
-    fontWeight: "bold",
-    fontSize: 18,
-    maxHeight: 60,
-    minHeight: 50,
-    marginTop: 20,
-  },
+const globalStyle=StyleSheet.create({
+    wholeScreen: {
+        backgroundColor: '#fff',
+        height: '100%',
+      },
+      buttons: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        justifyContent: 'space-evenly',
+      },
+      container: {
+        position: 'absolute',
+        bottom: 0,
+        width: "100%",
+        borderWidth: 5,
+        borderRadius: 999,
+        backgroundColor: '#fff',
+        padding: 2,
+      },
+      icon: {
+        width: 36,
+        height: 36,
+        alignSelf: 'center'
+      },
+      headerButtonRow: {
+        alignContent: 'space-between',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+      },
+      headerButtonStyle: {
+        flex: 1,
+        backgroundColor: 'black',
+        borderRadius: 20,
+        paddingVertical: 8,
+        paddingHorizontal: 20,
+        marginHorizontal: 4,
+      },
+      headerButtonText:{
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 20,
+        textAlign: 'center',
+      },
+      headerDivider: {
+        borderBottomColor: 'black',
+        borderBottomWidth: StyleSheet.hairlineWidth,
+      },
+      headerText: {
+        fontSize: 45,
+        fontWeight: 'bold',
+        textAlign: 'center',
+      },
+      inputContainer: {
+        borderWidth: 7,
+        borderRadius: 20,
+        paddingVertical: 5,
+        paddingHorizontal: 25,
+      },
 })
 
 export default globalStyle;
