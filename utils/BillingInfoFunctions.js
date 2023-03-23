880b85e01010aacbedd6df2b40023ab3da7f2db7
M-Valentino <mike9377mike9377@gmail.com>
2023-03-22 21:35:33 -0400

    #76 fixed bug where users can enter expired cards

--- utils/BillingInfoFunctions.js
+++ utils/BillingInfoFunctions.js
@@ -32,9 +32,46 @@ function cardNumberIsValidOrEmpty(cardNumber) {
   }
 }
 
-function expiryIsValidOrEmpty(expiry) {
-  if (expiry.match("(0[1-9]|1[0-2])\/[0-9][0-9]") || expiry.length === 0) {
+/**
+ * Function to check if card expiry is expired by comparing it against the current date.
+ * @param {*} expiry the date to check
+ * @returns true if expiry is not expired, else false
+ */
+function cardIsNotExpired(expiry) {
+  const date = new Date();
+  // Expiry dates have its year listed in two digits.
+  let currentYear = date.getFullYear().toString().substring(2, 4);
+  // Months in JavaScript are numbered starting at 0.
+  let currentMonth = date.getMonth() + 1;
+  let expiryMonth = expiry.substring(0, 2);
+  let expiryYear = expiry.substring(3, 5);
+
+  // An expiry month can't be compared with the current month if it has a zero prepended.
+  if (expiryMonth.charAt(0) === "0") {
+    expiryMonth = expiryMonth.substring(1, 2);
+  }
+  if (
+    expiryYear > currentYear ||
+    (expiryYear === currentYear && expiryMonth > currentMonth)
+  ) {
     return true;
+  } else {
+    Alert.alert("The card you have entered is expired.");
+  }
+  return false;
+}
+
+/**
+ * Function to check wether an expiry is formatted correctly and not expired, or just
+ * empty.
+ * @param {*} expiry the card expiration date to check.
+ * @returns true if expiry is valid or empty, else false.
+ */
+function expiryIsValidOrEmpty(expiry) {
+  if (expiry.match("(0[1-9]|1[0-2])/[0-9][0-9]") || expiry.length === 0) {
+    if (cardIsNotExpired(expiry)) {
+      return true;
+    }
   } else {
     Alert.alert("The expiry you inputed is formatted incorrectly.");
   }
