889fe79b46a059e158df496ac0f64d4258315423
Zt572 <ztyoung@email.sc.edu>
2023-04-19 14:00:01 -0400

    Implement tests for automatic sort menu closing #145

--- e2e/searchScreenSort.test.js
+++ e2e/searchScreenSort.test.js
@@ -27,4 +27,40 @@ describe('Example', () => {
     await element(by.id('Test_SortSubmenuOpacity')).longPress();
     await expect(element(by.id('Test_SortSubmenu'))).not.toExist();
   });
+
+  /* Successfully exit the menu after pressing any of the sorting options */
+  it('should remove the submenu after pressing lowest price sort', async () => {
+    await element(by.id('Test_SortButton')).longPress();
+    await waitFor(element(by.id('Test_SortSubmenu'))).toExist().withTimeout(1000);
+    await element(by.id('Test_SortLowestPriceButton')).longPress();
+    await expect(element(by.id('Test_SortSubmenu'))).not.toExist();
+  });
+
+  it('should remove the submenu after pressing highest price sort', async () => {
+    await element(by.id('Test_SortButton')).longPress();
+    await waitFor(element(by.id('Test_SortSubmenu'))).toExist().withTimeout(1000);
+    await element(by.id('Test_SortHighestPriceButton')).longPress();
+    await expect(element(by.id('Test_SortSubmenu'))).not.toExist();
+  });
+
+  it('should remove the submenu after pressing alphabetical sort', async () => {
+    await element(by.id('Test_SortButton')).longPress();
+    await waitFor(element(by.id('Test_SortSubmenu'))).toExist().withTimeout(1000);
+    await element(by.id('Test_SortA-ZButton')).longPress();
+    await expect(element(by.id('Test_SortSubmenu'))).not.toExist();
+  });
+
+  it('should remove the submenu after pressing reverse alphabetical sort', async () => {
+    await element(by.id('Test_SortButton')).longPress();
+    await waitFor(element(by.id('Test_SortSubmenu'))).toExist().withTimeout(1000);
+    await element(by.id('Test_SortZ-AButton')).longPress();
+    await expect(element(by.id('Test_SortSubmenu'))).not.toExist();
+  });
+
+  it('should remove the submenu after pressing unit price sort', async () => {
+    await element(by.id('Test_SortButton')).longPress();
+    await waitFor(element(by.id('Test_SortSubmenu'))).toExist().withTimeout(1000);
+    await element(by.id('Test_SortUnitPriceButton')).longPress();
+    await expect(element(by.id('Test_SortSubmenu'))).not.toExist();
+  });
 });
